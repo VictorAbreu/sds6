715ca1a66ff840456bac6e41cde52f759762b6e0
Victor Abreu <vlabreu@outlook.com>
2022-01-10 14:32:15 -0300

    Project created

---
+++ backend/src/main/java/br/com/vitt/vdmovie/VdmovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.vitt.vdmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VdmovieApplication.class, args);
+	}
+
+}
